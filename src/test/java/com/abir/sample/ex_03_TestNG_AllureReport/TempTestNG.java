add36201441a1aa9b8d5f9aa7e3797d7af29f29e
Bachchakabilbano93 <abirbhattacharya7@gmail.com>
2025-08-06 18:23:21 +0530

    5th August 2025 Class TestNG Testcases

---
+++ src/test/java/com/abir/sample/ex_03_TestNG_AllureReport/TempTestNG.java
@@ -0,0 +1,11 @@
+package com.abir.sample.ex_03_TestNG_AllureReport;
+
+import org.testng.annotations.Test;
+
+public class TempTestNG {
+
+    @Test
+    public void test_abir(){
+        System.out.println("Hello");
+    }
+}
